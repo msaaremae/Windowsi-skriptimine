66478834e194bfbe58ed52f796b34baacdb59d85
Martin Silver Saaremäe <m.saaremae@voco.ee>
2022-11-08 13:29:46 +0200

    Lokaalne kasutaja etteantud parooliga ja nimega

---
+++ lokaalsed_kasutajad.ps1
@@ -0,0 +1,3 @@
+﻿$KasutajaParool = ConvertTo-Securestring "Parool1!" -AsPlainText -Force
+
+New-LocalUser "kasutaja1" -Password $KasutajaParool -FullName "Esimene Kasutaja" -Description "Local Acount - kasutaja1"
